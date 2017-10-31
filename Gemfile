a7bc3208c832a2d0a11a6ea98d90694f59bff372
wangboguo <wangboguo@wangboguodeMacBook-Pro.local>
2017-10-31 11:54:26 +0800

    welcome page and login/logout/signup

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'bootstrap-sass'
 
 # devise
 gem 'devise'
+# jquery
+gem "jquery-rails"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
+//= require bootstrap/dropdown
+//= require bootstrap/alert
 //= require_tree .
